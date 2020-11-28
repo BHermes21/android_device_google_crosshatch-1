c08a032ebcf7cd7a359e28d04ad1e0e76851a31a
BHermes21 <BHermes21@sbcglobal.net>
2020-11-28 14:21:04 -0600

    Update BoardConfig-common.mk

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -19,6 +19,7 @@ include build/make/target/board/BoardConfigMainlineCommon.mk
 TARGET_BOARD_PLATFORM := sdm845
 TARGET_BOARD_INFO_FILE := device/google/crosshatch/board-info.txt
 USES_DEVICE_GOOGLE_B1C1 := true
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
 
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
